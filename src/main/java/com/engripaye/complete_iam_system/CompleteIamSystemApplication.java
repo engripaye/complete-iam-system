c2a9bcca5822e32f24aff770f1fa4d856b9a8d89
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-15 03:34:45 +0100

    first commit

---
+++ src/main/java/com/engripaye/complete_iam_system/CompleteIamSystemApplication.java
@@ -0,0 +1,13 @@
+package com.engripaye.complete_iam_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteIamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteIamSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engripaye/complete_iam_system/CompleteIamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engripaye.complete_iam_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteIamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
